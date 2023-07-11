3d33222b92cc34ab77e9ae93004e2fa85d24bfa3
João Alberto <jancunha@gmail.com>
2023-07-11 20:24:27 +0000

    hello daniela

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main()  {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello Net Haus</h1>"))
+		w.Write([]byte("<h1>Hello Daniela</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
